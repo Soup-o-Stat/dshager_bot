5130161c9f3464f9a785f0af772653e13e98f976
Soup-o-Stat <mr.drk.knght@gmail.com>
2025-10-20 16:50:32 +0300

    new gifs and text prints

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import sys
 from datetime import datetime, timezone
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from termcolor import cprint
 
-RESPONSE_CHANCE = 0.13
+RESPONSE_CHANCE = 0.15
 FILES_FOLDER = "videos"
 START_TIME = datetime.now(timezone.utc)
 
@@ -25,22 +26,23 @@ async def bot_loop(token):
 
     @dp.message(Command("start"))
     async def cmd_start(message: types.Message):
-        print(f"[BOT] {message.from_user.id}: {message.text}")
+        cprint(f"[BOT]: {message.text}", "light_yellow")
         await message.answer(
             "Привет, я Джагернаут!\n"
             "Я бот, который будет иногда отвечать на ваши сообщения забавными видео, фото и гифками!\n"
+            "Я эффективен в чатах, добавляй меня скорее --> @dshager_bot\n"
             "Подписывайся на мой канал: https://t.me/dshager_channel\n"
             "Ты можешь посмотреть список моих комманд, написав /help !"
         )
 
     @dp.message(Command("support"))
     async def cmd_support(message: types.Message):
-        print(f"[BOT] {message.from_user.id}: {message.text}")
+        cprint(f"[BOT]: {message.text}", "light_yellow")
         await message.answer("Ты можешь поддержать меня тут: https://dalink.to/soup_o_stat")
 
     @dp.message(Command("help"))
     async def cmd_help(message: types.Message):
-        print(f"[BOT] {message.from_user.id}: {message.text}")
+        cprint(f"[BOT]: {message.text}", "light_yellow")
         await message.answer(
             "Список команд:\n"
             "/start - приветственное сообщение\n"
@@ -51,11 +53,11 @@ async def bot_loop(token):
 
     @dp.message(Command("send"))
     async def cmd_send(message: types.Message):
-        print(f"[BOT] {message.text}")
+        cprint(f"[BOT] {message.text}", "light_yellow")
         file_path = get_random_file()
         if file_path:
             ext = os.path.splitext(file_path)[1].lower()
-            print(f">> Sending file: {file_path}, {message.date}, {message.message_id}")
+            cprint(f">> Sending file: {file_path}, {message.date}, {message.message_id}", "light_green")
             if ext in [".jpg", ".jpeg", ".png", ".webp"]:
                 await message.reply_photo(types.FSInputFile(file_path))
             elif ext in [".mp4", ".mov", ".avi", ".mkv"]:
@@ -69,14 +71,14 @@ async def bot_loop(token):
         if msg_date.tzinfo is None:
             msg_date = msg_date.replace(tzinfo=timezone.utc)
         if msg_date < START_TIME:
-            print(f"Lost message (id={message.message_id}, date={msg_date})")
+            cprint(f"Lost message (id={message.message_id}, date={msg_date})", "yellow")
             return
 
         if random.random() < RESPONSE_CHANCE:
             file_path = get_random_file()
             if file_path:
                 ext = os.path.splitext(file_path)[1].lower()
-                print(f">> Sending file: {file_path}, {message.date}, {message.message_id}")
+                cprint(f">> Sending file: {file_path}, {message.date}, {message.message_id}", "light_green")
                 if ext in [".jpg", ".jpeg", ".png", ".webp"]:
                     await message.reply_photo(types.FSInputFile(file_path))
                 elif ext in [".mp4", ".mov", ".avi", ".mkv"]:
@@ -84,20 +86,19 @@ async def bot_loop(token):
                 else:
                     await message.reply_document(types.FSInputFile(file_path))
 
-    print("Bot has been activated!")
+    cprint("Bot has been activated!", "green")
     await dp.start_polling(bot)
 
 async def listen_exit():
     while True:
         cmd = await asyncio.to_thread(input, "")
         if cmd.strip().lower() == "exit":
-            print("Exit command received. Shutting down...")
+            cprint("Exit command received. Shutting down...", "red")
             exit()
 
 async def main():
     if len(sys.argv) < 2:
-        print("Ошибка: укажите API токен в аргументах запуска.")
-        print("Пример: python main.py <API_TOKEN>")
+        cprint("Error: no API token!", "red")
         sys.exit(1)
 
     token = sys.argv[1].strip()
